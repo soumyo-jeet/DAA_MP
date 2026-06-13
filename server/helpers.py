37697a8f1528540b53822443b9f810093faa853f
shomoz <shourjya05@gmail.com>
2026-06-13 19:14:18 +0530

    Prerequisite checker implemented using DFS. The file structure is mentioned in the docc string. The parsing of the data is strictly followed by the mentioned file structure.

--- server/helpers.py
+++ server/helpers.py
@@ -45,4 +45,96 @@ A map of selected course ids to the prerequisites wrt a.txt
     .
     . 
 }
+Structure of the txt file as follows---
+
+Selected: CS301,CS302
+Prereq: CS301:CS201,CS202 | CS302:CS101 | CS201:CS100
+Completed: CS100,CS101
 '''
+
+def load_files(file_name):
+
+    try:
+        with open(file_name,'r') as file:
+            line=file.readlines()
+            
+        lines=[]
+        for l in line:
+            if l.strip():
+                lines.append(l)
+
+        select=lines[0].split(":")[1].strip()
+        preq=lines[1].split(":",1)[1].strip()
+        complete=lines[2].split(":")[1].strip()
+
+
+        if select.lower()=="none":
+            selected=[]
+        else:
+            selected=[x.strip() for x in select.split(",")]
+
+        prerequisite={}
+
+        for item in preq.split("|"):
+            course,preres=item.strip().split(":")
+
+            if preres.lower()=="none":
+                prerequisite[course.strip()]=[]
+            else:
+                prerequisite[course.strip()]=[x.strip() for x in preres.split(",")]
+
+
+        if complete.lower()=="none":
+            completed=[]
+        else:
+            completed=[x.strip() for x in complete.split(",")]
+
+
+        return selected,prerequisite,completed
+    except Exception as e:
+        return{
+            "Unexpected Error":str(e)
+        }
+
+
+
+def eligibility(selected,prerequisite,completed):
+
+    required=set()
+    stack=selected[:]
+    while len(stack)>0 :
+
+        course=stack.pop()
+        if course in prerequisite:
+            for prereq in prerequisite[course]:
+                if prereq not in required:
+                    required.add(prereq)
+                    stack.append(prereq)
+    
+
+    missing=[]
+    for course in required:
+        if course not in completed:
+            missing.append(course)
+
+
+    if len(missing)==0:
+        return{
+            "Eligible": True,
+            "Required Course": "None"
+        }
+    
+    return{
+        "Eligible": False,
+        "Required Course": missing
+    }
+
+        
+
+s,p,c=load_files("sample.txt")
+print(eligibility(s,p,c))
+
+
+    
+
+
